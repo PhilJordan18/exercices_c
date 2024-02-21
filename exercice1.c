a4a7ff0dc4dcf89ef8323a006e227fc14b8dde3d
PhilJordan18 <philippemonf@yahoo.com>
2024-02-21 13:58:54 -0500

    Interchanger des valeurs entières

--- exercice1.c
+++ exercice1.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+void interchangerValeur(int *varUn, int *varDeux);
+
 int main(){
-    printf("Hello World");
+    int numUn, numDeux;
+
+    printf("Veillez saisir votre premier chiffre : ");
+    scanf("%d", &numUn);
+
+    printf("Veillez saisir votre second chiffre : ");
+    scanf("%d", &numDeux);
+
+    printf("Valuers actuelles :\nPremier Chiffre : %d     Second Chiffre : %d\n\n", numUn, numDeux );
+
+    interchangerValeur(&numUn, &numDeux);
+    printf("Valuers interchangées :\nPremier Chiffre : %d     Second Chiffre : %d\n", numUn, numDeux );
     return 0;
+}
+
+void interchangerValeur(int *varUn, int *varDeux){
+    int stockTemporaire = *varUn;
+    *varUn = *varDeux;
+    *varDeux = stockTemporaire;
 }
